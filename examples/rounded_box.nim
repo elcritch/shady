f9e086f202d9c807a48d470860b052fc3b944cf8
Jaremy Creechley <creechley@gmail.com>
2025-06-13 02:45:11 -0600

    tweaks

--- examples/rounded_box.nim
+++ examples/rounded_box.nim
@@ -42,11 +42,13 @@ proc roundedBoxShader(fragColor: var Vec4, uv: Vec2, posColor: Uniform[Vec4], ne
   # Color based on distance
   if sd < 0.0:
     fragColor = posColor
-    discard
+    let f = min(1.0, dropShadow(1 - sd, stdDevFactor=1.0/2.2, spread=10.0, factor=10.0))
+    fragColor = mix(posColor, negColor, f)
   else:
     fragColor = negColor
     let f = min(1.0, dropShadow(sd, stdDevFactor=1.0/2.2, spread=10.0, factor=10.0))
     fragColor = mix(posColor, negColor, f)
+    discard
 
 # Compile to a GPU shader:
 var shader = toGLSL(roundedBoxShader)
